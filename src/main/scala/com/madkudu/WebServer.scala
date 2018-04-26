be07426409ff0718e51f0278518b428baecdf917
Thomas Wilgenbus <thomas.wilgenbus@axa-groupsolutions.com>
2018-04-26 16:30:54 +0200

    Add caching system (x3 faster performance)

--- src/main/scala/com/madkudu/WebServer.scala
+++ src/main/scala/com/madkudu/WebServer.scala
@@ -1,27 +1,23 @@
 package com.madkudu
 
-import java.util.Date
+import java.util.Properties
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives
-import akka.http.scaladsl.server.Directives._
-import com.madkudu._
-import org.apache.spark.rdd._
 import akka.stream.ActorMaterializer
-import com.madkudu.data.ActionDataModels.{ActionData, JsonSupport}
-import org.apache.spark.SparkConf
 import com.datastax.spark.connector._
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.cassandra._
-import org.apache.spark.SparkContext._
-
-import scala.io.StdIn
-import spray.json._
-import org.joda.time.{DateTime, LocalDate}
+import com.madkudu.data.ActionDataModels.{ActionData, JsonSupport}
+import org.apache.commons.lang.StringEscapeUtils.escapeJava
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
+import spray.json._
+
+import scala.concurrent.Future
+import scala.io.StdIn
 
 object WebServer extends Directives with JsonSupport {
 
@@ -32,44 +28,52 @@ object WebServer extends Directives with JsonSupport {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
+    val props = new Properties()
+    props.put("bootstrap.servers", kafkaBroker)
+    props.put("client.id", "UserBehaviorProducer")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](props)
+
+
     val sparkConf = new SparkConf().setAppName("Behaviors")
     sparkConf.setIfMissing("spark.master", "local[5]") //"spark://127.0.0.1:7077")
     //    sparkConf.setIfMissing("spark.checkpoint.dir", checkpointDir)
     sparkConf.setIfMissing("spark.cassandra.connection.host", "127.0.0.1")
     val sc = new SparkContext(sparkConf)
     val route =
-      path("v1" / "user" / Segment) { userId =>
+      pathPrefix("v1" ) {
+        path("user" / Segment) { userId =>
           get {
-            val views = sc.cassandraTable(cassandraKeyspace, cassandraTableDailyViews)
-              .select("views")
-              .as((views: Int) => views)
+            val userBehavior = sc.cassandraTable(cassandraKeyspace, cassandraTableDailyViews)
+              .select("day", "name", "views")
+              .as((day: String, name: String, views: Int) => (day, name, views))
               .where("day >= ? AND uid = ?", ISODateTimeFormat.date().print(DateTime.now.minusDays(7)), userId)
-              .fold(0)(_ + _)
+              .cache()
 
-            val activeDays = sc.cassandraTable(cassandraKeyspace, cassandraTableDailyViews)
-                .select("day")
-                .as((day: String) => day)
-                .where("day >= ? AND uid = ?", ISODateTimeFormat.date().print(DateTime.now.minusDays(7)), userId)
-                .distinct()
-                .count()
+            if (!userBehavior.isEmpty()) {
 
-            val mostViewedPage = sc.cassandraTable(cassandraKeyspace, cassandraTableDailyViews)
-                .select("views", "name")
-                .as((views: Int, name: String) => (name, views))
-                .where("day >= ? AND uid = ?", ISODateTimeFormat.date().print(DateTime.now.minusDays(7)), userId)
+              val views = userBehavior.map(_._3).fold(0)(_ + _)
+
+              val activeDays = userBehavior.distinct().count()
+
+              val mostViewedPage = userBehavior.map(d => (d._2, d._3))
                 .foldByKey(0)(_ + _)
                 .max()(Ordering[Int].on(_._2))._1
 
-            complete {
-              Map(
-                "user_id" -> userId.toJson,
-                "number_pages_viewed_in_the_last_7_days" -> views.toJson,
-                "time_spent_on_site_in_last_7_days" -> 0.toJson,
-                "number_of_days_active_in_last_7_days" -> activeDays.toJson,
-                "most_viewed_page_in_last_7_days" -> mostViewedPage.toJson
-              ).toJson
-            }
+              complete {
+                Map(
+                  "user_id" -> userId.toJson,
+                  "number_pages_viewed_in_the_last_7_days" -> views.toJson,
+                  "time_spent_on_site_in_last_7_days" -> 0.toJson,
+                  "number_of_days_active_in_last_7_days" -> activeDays.toJson,
+                  "most_viewed_page_in_last_7_days" -> mostViewedPage.toJson
+                ).toJson
+              }
+            } else complete((NotFound, s"No data for user $userId"))
           }
+        
       }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
